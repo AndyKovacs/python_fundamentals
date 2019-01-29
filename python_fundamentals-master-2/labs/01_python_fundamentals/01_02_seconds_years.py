b9cd1a117a9b76c5cd52b3599a3b019d8d81aaf7
Firstname Lastname <your_email@unibz.it>
2019-01-29 10:00:42 +0100

    initial commit

---
+++ python_fundamentals-master-2/labs/01_python_fundamentals/01_02_seconds_years.py
@@ -0,0 +1,7 @@
+
+x=1
+y = x*60*60*24*365
+print("one year has" y"seconds")
+From the previous example, move your calculation of how many seconds in a year to a python executable script.
+
+'''
---
+++ yearinseconds.py
@@ -0,0 +1,3 @@
+x=1
+y = x*60*60*24*365
+print ("one year has" % "seconds",y)
